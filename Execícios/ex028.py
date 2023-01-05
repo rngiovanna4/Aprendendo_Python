b4ec436b6f3c01a45c513358dc48bd092d00552c
rngiovanna4 <118777919+rngiovanna4@users.noreply.github.com>
2023-01-05 17:15:19 -0300

    detalhes...

--- Execícios/ex028.py
+++ Execícios/ex028.py
@@ -1,11 +1,14 @@
 '''Um jogo onde o computador pensa em um número e o usuário tenta adivinha qual foi o número escolhido. O programa deve escrever na tela se o usuário ganhou ou perdeu'''
 from random import randint
+from time import sleep
 num = randint(1,5)
-print(num)
-chute = int(input('Digite um número de 0 a 5 e tente adivinhar o número que o computador está pensando'))
-print(num)
+print('Vamos jogar?')
+chute = int(input('Digite um número de 0 a 5 e tente adivinhar em que número eu pensando!\n'))
+#print(num)
+print('ANALISANDO A SUA RESPOSTA...')
+sleep(3)
 
 if num==chute:
-    print('Você ganhou. O número é {}'.format(num))
+    print('O número que eu pensei é {}. Você venceu!'.format(num))
 else:
-    print('Você perdeu. O número é {}'.format(num))
+    print('Perdeu! Pensei no número é {}. Mais sorte na próxima! hehe/'.format(num))
